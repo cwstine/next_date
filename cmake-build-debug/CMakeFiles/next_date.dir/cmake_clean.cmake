76c2b668eb753e13683725dc740681fe0c88e8dd
Christopher Stine <cwstine@email.wm.edu>
2021-02-13 14:25:32 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/next_date.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/next_date.dir/main.cpp.obj"
+  "libnext_date.dll.a"
+  "next_date.exe"
+  "next_date.exe.manifest"
+  "next_date.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/next_date.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/next_date.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for next_date.
+# This may be replaced when dependencies are built.
